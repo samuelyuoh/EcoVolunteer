3bb6d1d926a196fe9dca4e9fadb27a4c17f9dc82
samuelyuoh <samuelyuoh@gmail.com>
2023-01-07 10:31:02 +0800

    created session & booking service

---
+++ MyCompany/MyCompany/Services/BookingService.cs
@@ -0,0 +1,34 @@
+﻿using MyCompany.Models;
+
+namespace MyCompany.Services
+{
+    public class BookingService
+    {
+        private readonly MyDbContext _context;
+        public BookingService(MyDbContext context)
+        {
+            _context = context;
+        }
+        public List<Booking> GetAll()
+        {
+            _context.Database.EnsureCreated();
+            return _context.Bookings.OrderBy(m => m.BookingId).ToList();
+        }
+        public Booking? GetSessionById(int id)
+        {
+            Booking? booking = _context.Bookings.FirstOrDefault(
+            x => x.BookingId.Equals(id));
+            return booking;
+        }
+        public void AddBooking(Booking booking)
+        {
+            _context.Bookings.Add(booking);
+            _context.SaveChanges();
+        }
+        public void UpdateBooking(Booking booking)
+        {
+            _context.Bookings.Update(booking);
+            _context.SaveChanges();
+        }
+    }
+}
