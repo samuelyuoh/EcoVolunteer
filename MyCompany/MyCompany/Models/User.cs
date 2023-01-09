545353d114c054adc20a66195156e8bdf80e7691
Kitsuneez <106949197+Kitsuneez@users.noreply.github.com>
2023-01-09 20:30:42 +0800

    accepted merge for user.cs

--- MyCompany/MyCompany/Models/User.cs
+++ MyCompany/MyCompany/Models/User.cs
@@ -5,44 +5,47 @@ using System.ComponentModel;
 
 namespace MyCompany.Models
 {
-    public class User
-    {
+	public class User
+	{
         [Required, Key]
         public int UserId { get; set; }
-        [Required, MaxLength(50)]
-        public string Name { get; set; } = string.Empty;
+		[Required, MaxLength(50)]
+		[Display(Name = "User ID")]
+		public string UserId { get; set; } = string.Empty;
+		[Required, MaxLength(50)]
+		public string Name { get; set; } = string.Empty;
 
-        [Required, Range(0, 120)]
-        public int Age { get; set; } = 0;
+		[Required, Range(0, 120)]
+		public int Age { get; set; } = 0;
 
-        [Required]
-        public Boolean Admin_Role { get; set; } = false;
+		[Required]
+		public Boolean Admin_Role { get; set; } = false;
 
-        [Required, MaxLength(100)]
-        public string Address { get; set; } = string.Empty;
+		[Required, MaxLength(100)]
+		public string Address { get; set; } = string.Empty;
 
-        [Required, StringLength(6)]
-        public int Postal { get; set; } = 0;
+		[Required, StringLength(6)]
+		public int Postal { get; set; } = 0;
 
-        [Required, StringLength(8)]
+		[Required, StringLength(8)]
         public string Phone { get; set; } = string.Empty;
 
-        [Required]
+		[Required]
         public string Email { get; set; } = string.Empty;
 
-        [Required]
+		[Required]
         public string password { get; set; } = string.Empty;
 
-        [MaxLength(50)]
-        public string? ImageURL { get; set; }
+		[MaxLength(50)]
+		public string? ImageURL { get; set; }
 
-        [Required]
-        public DateTime Date_Joined { get; set; } = DateTime.Now;
+		[Required]
+		public DateTime Date_Joined { get; set; } = DateTime.Now;
 
-        [Required]
-        public Boolean Ban_Status { get; set; } = false;
+		[Required]
+		public Boolean Ban_Status { get; set; } = false;
 
-        [Required]
-        public int Credits { get; set; } = 0;
-    }
+		[Required]
+		public int Credits { get; set; } = 0;
+	}
 }
